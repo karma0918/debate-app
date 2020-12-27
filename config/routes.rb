802587b5e6c6cf0889ec5f2dd46069fe019b2241
karma0918 <69901317+karma0918@users.noreply.github.com>
2020-12-27 21:03:04 +0900

    ルーティングエラー

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'users/index'
   root to: "users#index"
-  resources :users, only: index
+  resources :users, only: [:index]
   resources :room, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
