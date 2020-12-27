7f25245357303039c530703462a6aa584e35bb19
karma0918 <69901317+karma0918@users.noreply.github.com>
2020-12-27 14:26:48 +0900

    userindexの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'users/index'
   root to: "users#index"
+  resources :users, only: index
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
