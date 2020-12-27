a18142de4643767ccb51fd8250b3db8017437b4e
karma0918 <69901317+karma0918@users.noreply.github.com>
2020-12-27 14:47:58 +0900

    ルームの追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'users/index'
   root to: "users#index"
   resources :users, only: index
+  resources :room, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
