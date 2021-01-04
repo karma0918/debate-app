ae8b4a1fb9b3a7c383702f41a0e9946d62b6d837
karma0918 <69901317+karma0918@users.noreply.github.com>
2021-01-04 22:41:56 +0900

    userからroomに変更組

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'users/index'
-  root to: "users#index"
+  root to: "rooms#index"
   resources :users, only: [:index]
-  resources :room, only: [:new, :create]
+  resources :rooms, only: [:index, :new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
