176628f4cc377ea455d223d86240ada0bdd5cc38
karma0918 <69901317+karma0918@users.noreply.github.com>
2021-01-04 22:40:40 +0900

    userコントローラーからroomsコントローラーに移動

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,9 +1,25 @@
 class RoomsController < ApplicationController
-
-  def create
-
+  def index
+    @user = User.all
+     @rooms = Room.all
   end
   def new
-    
+    @room = Room.new
   end
+
+  def create
+    binding.pry
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def room_params
+    params.require(:room).permit(:name,:explanation,:category_id)
+  end
+
 end
