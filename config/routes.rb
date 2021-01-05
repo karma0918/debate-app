eb811c7f848a74467c9a266942ccaa834e1899ad
karma0918 <69901317+karma0918@users.noreply.github.com>
2021-01-05 20:58:18 +0900

    仮のメッセージのview

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   root to: "rooms#index"
   resources :users, only: [:index]
   resources :rooms, only: [:index, :new, :create] do
-    resources :messages, only: [:index]
+    resources :messages, only: [:index,:create]
+  end
+  
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
