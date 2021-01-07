72dfb1cb08676cc59859ee5c56b10375ca3a80bd
karma0918 <69901317+karma0918@users.noreply.github.com>
2021-01-08 00:39:22 +0900

    間違って削除

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -7,8 +7,7 @@ class RoomsController < ApplicationController
     @room = Room.new
   end
 
-  def create
-    binding.pry
+
     @room = Room.new(room_params)
     if @room.save
       redirect_to root_path
