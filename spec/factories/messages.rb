42aec3f516a5a228edc55c62f1c6937735a69d92
karma0918 <69901317+karma0918@users.noreply.github.com>
2021-01-13 22:02:01 +0900

    テストコード

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :message do
-    
+    content {"test"}
+    user_id {1}
+    room_id {1}
+    association :user
+    association :room
   end
 end
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @message = FactoryBot.build(:message)
+  end
+  it "contentが空では保存されない" do
+    @message.content = nil
+    @message.valid?
+    expect(@message.errors.full_messages).to include("Content can't be blank")
+  end
+  it "contentがあれば保存する" do
+    expect(@message).to be_valid
+  end
 end
