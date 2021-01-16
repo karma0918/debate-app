6e6ce9230d67332c42064cdd1dda1e2e12d3697c
karma0918 <69901317+karma0918@users.noreply.github.com>
2021-01-17 08:45:49 +0900

    initial commit

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,8 +2,9 @@ class Room < ApplicationRecord
 
   with_options presence: true do
     validates :name
-    validates :explanation
+    validates :explanation, length: { maximum: 256 }
     validates :category_id, numericality: { other_than: 1 }
+    validates :image
   end
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
