8ffc2a36f9bbe2b93e879d573af5105d46f9bc6b
karma0918 <69901317+karma0918@users.noreply.github.com>
2021-01-17 15:31:11 +0900

    jquaryのbootstrapの追加

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,4 +4,18 @@ const vue = require('./loaders/vue')
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
+
+environment.config.resolve.alias = { 'vue$':'vue/dist/vue.esm.js' }
+
+
+const webpack = require('webpack')
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: 'popper.js'
+  })
+)
+
 module.exports = environment
