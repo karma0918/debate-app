6e5d558b1abdd075e7f9eb3ba4ad4632b1c6a5ca
karma0918 <69901317+karma0918@users.noreply.github.com>
2021-01-20 23:03:24 +0900

    時間の表示やビューの変更

--- config/application.rb
+++ config/application.rb
@@ -6,11 +6,14 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
+
 module DebateApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
